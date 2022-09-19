6400e1ab2707ea9bd2a2b8609d9c58f17733b081
Kseniya <ksavierra@rambler.ru>
2022-09-19 11:00:53 +0500

    ешение 1й задачи

---
+++ les1.py
@@ -0,0 +1,13 @@
+# Задайте список из нескольких чисел. 
+# Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+list = list(map(int, input("Введите числа через пробел:\n").split()))
+
+def sumOddIndex(list):
+    sum = 0
+    for i in range(len(list)):
+        if i % 2 != 0:
+            sum += list[i]
+    print(f"Сумма равна: {sum}")
+
+sumOddIndex(list)
